9d810a63208cc7bdf5e4ccc93de206c6eb03367d
Kamran Ansari <crossthinking786@gmail.com>
2022-08-05 17:10:17 +0530

    completed Part08_06.Nicknames

--- part08-Part08_06.Nicknames/src/main/java/Nicknames.java
+++ part08-Part08_06.Nicknames/src/main/java/Nicknames.java
@@ -5,6 +5,14 @@ public class Nicknames {
 
     public static void main(String[] args) {
         // Do the operations required here!
+
+        HashMap<String, String> nickNameHashMap = new HashMap<>();
+
+        nickNameHashMap.put("matthew", "matt");
+        nickNameHashMap.put("michael", "mix");
+        nickNameHashMap.put("arthur", "artie");
+
+        System.out.println("matthew's nickname is " + nickNameHashMap.get("matthew"));
     }
 
 }
