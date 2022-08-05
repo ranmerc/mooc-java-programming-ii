713d32eb9eb41b059d4f28c2a0822c67501e2061
Kamran Ansari <crossthinking786@gmail.com>
2022-08-05 15:48:39 +0530

    completed Part08_03.LiquidContainers

--- part08-Part08_03.LiquidContainers/src/main/java/LiquidContainers.java
+++ part08-Part08_03.LiquidContainers/src/main/java/LiquidContainers.java
@@ -5,16 +5,57 @@ public class LiquidContainers {
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-
+        int firstContainer = 0;
+        int secondContainer = 0;
 
         while (true) {
-            System.out.print("> ");
+            System.out.println("First: " + firstContainer + "/100");
+            System.out.println("Second: " + secondContainer + "/100");
 
+            System.out.print("> ");
             String input = scan.nextLine();
+
             if (input.equals("quit")) {
                 break;
             }
 
+            String[] fields = input.split(" ");
+            String command = fields[0];
+            int amount = Integer.valueOf(fields[1]);
+
+            if (amount < 0) {
+                continue;
+            }
+
+            if (command.equals("add")) {
+                firstContainer += amount;
+
+                if (firstContainer > 100) {
+                    firstContainer = 100;
+                }
+            }
+
+            if (command.equals("move")) {
+                if (amount >= firstContainer) {
+                    secondContainer += firstContainer;
+                    firstContainer = 0;
+                } else {
+                    firstContainer -= amount;
+                    secondContainer += amount;
+                }
+
+                if (secondContainer > 100) {
+                    secondContainer = 100;
+                }
+            }
+
+            if (command.equals("remove")) {
+                secondContainer -= amount;
+
+                if (secondContainer < 0) {
+                    secondContainer = 0;
+                }
+            }
         }
     }
 
