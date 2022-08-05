9ebe546b0f08f97254efed5694fb9f2c0b9ab366
Kamran Ansari <crossthinking786@gmail.com>
2022-08-05 16:04:11 +0530

    completed Part08_04.LiquidContainers2

--- part08-Part08_04.LiquidContainers2/src/main/java/LiquidContainers2.java
+++ part08-Part08_04.LiquidContainers2/src/main/java/LiquidContainers2.java
@@ -5,16 +5,45 @@ public class LiquidContainers2 {
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-
+        Container firstContainer = new Container();
+        Container secondContainer = new Container();
 
         while (true) {
-            System.out.print("> ");
+            System.out.println("First: " + firstContainer);
+            System.out.println("Second: " + secondContainer);
 
+            System.out.print("> ");
             String input = scan.nextLine();
+
             if (input.equals("quit")) {
                 break;
             }
 
+            String[] fields = input.split(" ");
+            String command = fields[0];
+            int amount = Integer.valueOf(fields[1]);
+
+            if (amount < 0) {
+                continue;
+            }
+
+            if (command.equals("add")) {
+                firstContainer.add(amount);
+            }
+
+            if (command.equals("move")) {
+                if (amount >= firstContainer.contains()) {
+                    secondContainer.add(firstContainer.contains());
+                    firstContainer.remove(firstContainer.contains());
+                } else {
+                    secondContainer.add(amount);
+                    firstContainer.remove(amount);
+                }
+            }
+
+            if (command.equals("remove")) {
+                secondContainer.remove(amount);
+            }
         }
     }
 
