e16d7d685660b36b09615e5e5c9cdd229dbb9356
Kamran Ansari <crossthinking786@gmail.com>
2022-08-07 12:59:49 +0530

    completed Part09_02.PersonAndSubclasses

--- part09-Part09_02.PersonAndSubclasses/src/main/java/Main.java
+++ part09-Part09_02.PersonAndSubclasses/src/main/java/Main.java
@@ -1,8 +1,16 @@
+import java.util.ArrayList;
 
 public class Main {
 
     public static void main(String[] args) {
         // write your test code here
+        Student ollie = new Student("Ollie", "6381 Hollywood Blvd. Los Angeles 90028");
+        System.out.println(ollie);
     }
 
+    public static void printPersons(ArrayList<Person> persons) {
+        for (Person person : persons) {
+            System.out.println(person);
+        }
+    }
 }
