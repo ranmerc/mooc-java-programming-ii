7de47d2e13a4dd8ea8be98205087fbbf53c32788
Kamran Ansari <crossthinking786@gmail.com>
2022-08-10 16:36:55 +0530

    completed Part09_12.Animals

--- part09-Part09_12.Animals/src/main/java/Main.java
+++ part09-Part09_12.Animals/src/main/java/Main.java
@@ -4,6 +4,17 @@ public class Main {
     public static void main(String[] args) {
         // you can test how your classes work here
 
+        // Assigning a sub class object with super class reference object
+        Animal cat = new Cat("Michi");
+
+        // Cannot call purr() because it does not exist on super class (Animal)
+        // cat.purr();
+
+        // toString exists on super class
+        // so the super class reference object is able to call the method
+        // but it is overridden by sub class so instead of
+        // calling super class's toString, it calls sub class toString
+        cat.toString();
     }
 
 }
