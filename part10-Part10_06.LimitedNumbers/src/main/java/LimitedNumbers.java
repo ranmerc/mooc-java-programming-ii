dbfa536ac8945f388a0aa54b8668c106a45cd5af
Kamran Ansari <crossthinking786@gmail.com>
2022-08-11 22:48:23 +0530

    completed Part10_06.LimitedNumbers

--- part10-Part10_06.LimitedNumbers/src/main/java/LimitedNumbers.java
+++ part10-Part10_06.LimitedNumbers/src/main/java/LimitedNumbers.java
@@ -1,11 +1,27 @@
 
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.function.Function;
 
 public class LimitedNumbers {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        ArrayList<Integer> numberList = new ArrayList<>();
+
+        while (true) {
+            int number = Integer.valueOf(scanner.nextLine());
+
+            if (number < 0) {
+                break;
+            }
+
+            numberList.add(number);
+        }
+
+        numberList.stream()
+                .filter(n -> n >= 1 && n <= 5)
+                .forEach(n -> System.out.println(n));
 
     }
 }
