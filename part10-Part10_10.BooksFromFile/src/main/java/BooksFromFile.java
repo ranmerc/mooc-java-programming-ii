5cba626a9337c12302263cad617f5235bf0b33c2
Kamran Ansari <crossthinking786@gmail.com>
2022-08-11 23:30:17 +0530

    completed Part10_10.BooksFromFile

--- part10-Part10_10.BooksFromFile/src/main/java/BooksFromFile.java
+++ part10-Part10_10.BooksFromFile/src/main/java/BooksFromFile.java
@@ -7,11 +7,25 @@ import java.util.Scanner;
 import java.util.stream.Collectors;
 
 public class BooksFromFile {
-    
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         // test your method here
 
     }
 
+    public static List<Book> readBooks(String file) {
+        ArrayList<Book> bookList = new ArrayList<>();
+
+        try {
+            Files.lines(Paths.get(file))
+                    .map(line -> line.split(","))
+                    .map(parts -> new Book(parts[0], Integer.valueOf(parts[1]), Integer.valueOf(parts[2]), parts[3]))
+                    .forEach(book -> bookList.add(book));
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+
+        return bookList;
+    }
 }
