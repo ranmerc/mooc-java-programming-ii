347640d44812779ce9015dd222d266dbdf269b38
Kamran Ansari <crossthinking786@gmail.com>
2022-08-15 20:25:21 +0530

    completed Part11_05.ThePlayerAndTheBot

---
+++ part11-Part11_05.ThePlayerAndTheBot/src/main/java/Bot.java
@@ -0,0 +1,10 @@
+public class Bot extends Player {
+
+    public void play() {
+        System.out.println("A Bot is playing.");
+    }
+
+    public void addMove(String move) {
+        System.out.println("Move added to the bot");
+    }
+}
---
+++ part11-Part11_05.ThePlayerAndTheBot/src/main/java/Player.java
@@ -0,0 +1,12 @@
+public class Player {
+
+    private String name;
+
+    public void play() {
+        System.out.println(name + " is playing.");
+    }
+
+    public void printName() {
+        System.out.println("Player name: " + name);
+    }
+}
