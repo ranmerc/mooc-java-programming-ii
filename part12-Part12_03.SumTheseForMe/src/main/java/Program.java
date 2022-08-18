10ed07d986f42efd35655a54b3b67e8376acd391
Kamran Ansari <crossthinking786@gmail.com>
2022-08-18 19:43:30 +0530

    completed Part12_03.SumTheseForMe

--- part12-Part12_03.SumTheseForMe/src/main/java/Program.java
+++ part12-Part12_03.SumTheseForMe/src/main/java/Program.java
@@ -9,4 +9,23 @@ public class Program {
 
     }
 
+    public static int sum(int[] array, int fromWhere, int toWhere, int smallest, int largest) {
+        if (fromWhere < 0) {
+            fromWhere = 0;
+        }
+
+        if (toWhere > array.length) {
+            toWhere = array.length;
+        }
+
+        int sum = 0;
+
+        for (int i = fromWhere; i < toWhere; i++) {
+            if (array[i] < largest && array[i] > smallest) {
+                sum += array[i];
+            }
+        }
+
+        return sum;
+    }
 }
