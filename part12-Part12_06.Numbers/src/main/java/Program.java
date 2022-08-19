0bcef49d967fce47aa90188e43a450aada6088ad
Kamran Ansari <crossthinking786@gmail.com>
2022-08-19 19:48:33 +0530

    completed Part12_06.Numbers

--- part12-Part12_06.Numbers/src/main/java/Program.java
+++ part12-Part12_06.Numbers/src/main/java/Program.java
@@ -6,6 +6,15 @@ public class Program {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        Random randomObj = new Random();
+
+        System.out.println("How many random numbers should be printed?");
+        int times = Integer.valueOf(scanner.nextLine());
+
+        for (int i = 0; i < times; i++) {
+            int randomNumber = randomObj.nextInt(11);
+            System.out.println(randomNumber);
+        }
     }
 
 }
