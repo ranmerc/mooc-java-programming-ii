169637613763d96f3ee17917d77bee9cb850e7e0
Kamran Ansari <crossthinking786@gmail.com>
2022-08-19 21:26:58 +0530

    completed Part12_09.ArrayAsAString

--- part12-Part12_09.ArrayAsAString/src/main/java/Program.java
+++ part12-Part12_09.ArrayAsAString/src/main/java/Program.java
@@ -5,4 +5,17 @@ public class Program {
         // Test your method here
     }
 
+    public static String arrayAsString(int[][] array) {
+        StringBuilder toReturn = new StringBuilder();
+
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                toReturn.append(array[i][j]);
+            }
+
+            toReturn.append("\n");
+        }
+
+        return toReturn.toString();
+    }
 }
